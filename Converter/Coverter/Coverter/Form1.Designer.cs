a458a856ba3c81dedffbdb7623155b96cf8c2277
LaVannak <Vannak.la@gmail.com>
2017-04-26 13:40:59 +0800

    Add files via upload

---
+++ Converter/Coverter/Coverter/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Coverter
+{
+    partial class FrmConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.NumInput = new System.Windows.Forms.NumericUpDown();
+            this.cmbWstern = new System.Windows.Forms.ComboBox();
+            this.cmbAsia = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbTemperature = new System.Windows.Forms.RadioButton();
+            this.rdbValume = new System.Windows.Forms.RadioButton();
+            this.rdbArea = new System.Windows.Forms.RadioButton();
+            this.rdbLength = new System.Windows.Forms.RadioButton();
+            this.rdbWeiht = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.NumInput)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "To";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(172, 159);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // NumInput
+            // 
+            this.NumInput.DecimalPlaces = 2;
+            this.NumInput.Location = new System.Drawing.Point(13, 151);
+            this.NumInput.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.NumInput.Name = "NumInput";
+            this.NumInput.Size = new System.Drawing.Size(120, 20);
+            this.NumInput.TabIndex = 6;
+            this.NumInput.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumInput.ValueChanged += new System.EventHandler(this.NumInput_ValueChanged);
+            // 
+            // cmbWstern
+            // 
+            this.cmbWstern.FormattingEnabled = true;
+            this.cmbWstern.Items.AddRange(new object[] {
+            "adf"});
+            this.cmbWstern.Location = new System.Drawing.Point(11, 124);
+            this.cmbWstern.Name = "cmbWstern";
+            this.cmbWstern.Size = new System.Drawing.Size(121, 21);
+            this.cmbWstern.TabIndex = 1;
+            this.cmbWstern.SelectedIndexChanged += new System.EventHandler(this.cmbWstern_SelectedIndexChanged);
+            // 
+            // cmbAsia
+            // 
+            this.cmbAsia.FormattingEnabled = true;
+            this.cmbAsia.Items.AddRange(new object[] {
+            "df"});
+            this.cmbAsia.Location = new System.Drawing.Point(164, 124);
+            this.cmbAsia.Name = "cmbAsia";
+            this.cmbAsia.Size = new System.Drawing.Size(121, 21);
+            this.cmbAsia.TabIndex = 7;
+            this.cmbAsia.SelectedIndexChanged += new System.EventHandler(this.cmbAsia_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbTemperature);
+            this.groupBox1.Controls.Add(this.rdbValume);
+            this.groupBox1.Controls.Add(this.rdbArea);
+            this.groupBox1.Controls.Add(this.rdbLength);
+            this.groupBox1.Controls.Add(this.rdbWeiht);
+            this.groupBox1.Location = new System.Drawing.Point(8, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 82);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Type of Measurement Units";
+            // 
+            // rdbTemperature
+            // 
+            this.rdbTemperature.AutoSize = true;
+            this.rdbTemperature.Location = new System.Drawing.Point(100, 48);
+            this.rdbTemperature.Name = "rdbTemperature";
+            this.rdbTemperature.Size = new System.Drawing.Size(85, 17);
+            this.rdbTemperature.TabIndex = 4;
+            this.rdbTemperature.Text = "Temperature";
+            this.rdbTemperature.UseVisualStyleBackColor = true;
+            this.rdbTemperature.CheckedChanged += new System.EventHandler(this.rdbTemperature_CheckedChanged);
+            // 
+            // rdbValume
+            // 
+            this.rdbValume.AutoSize = true;
+            this.rdbValume.Location = new System.Drawing.Point(8, 48);
+            this.rdbValume.Name = "rdbValume";
+            this.rdbValume.Size = new System.Drawing.Size(60, 17);
+            this.rdbValume.TabIndex = 3;
+            this.rdbValume.Text = "Volume";
+            this.rdbValume.UseVisualStyleBackColor = true;
+            this.rdbValume.CheckedChanged += new System.EventHandler(this.rdbValume_CheckedChanged);
+            // 
+            // rdbArea
+            // 
+            this.rdbArea.AutoSize = true;
+            this.rdbArea.Location = new System.Drawing.Point(205, 24);
+            this.rdbArea.Name = "rdbArea";
+            this.rdbArea.Size = new System.Drawing.Size(47, 17);
+            this.rdbArea.TabIndex = 2;
+            this.rdbArea.Text = "Area";
+            this.rdbArea.UseVisualStyleBackColor = true;
+            this.rdbArea.CheckedChanged += new System.EventHandler(this.rdbArea_CheckedChanged);
+            // 
+            // rdbLength
+            // 
+            this.rdbLength.AutoSize = true;
+            this.rdbLength.Location = new System.Drawing.Point(10, 25);
+            this.rdbLength.Name = "rdbLength";
+            this.rdbLength.Size = new System.Drawing.Size(58, 17);
+            this.rdbLength.TabIndex = 1;
+            this.rdbLength.Text = "Length";
+            this.rdbLength.UseVisualStyleBackColor = true;
+            this.rdbLength.CheckedChanged += new System.EventHandler(this.rdbLength_CheckedChanged);
+            // 
+            // rdbWeiht
+            // 
+            this.rdbWeiht.AutoSize = true;
+            this.rdbWeiht.Location = new System.Drawing.Point(100, 24);
+            this.rdbWeiht.Name = "rdbWeiht";
+            this.rdbWeiht.Size = new System.Drawing.Size(59, 17);
+            this.rdbWeiht.TabIndex = 0;
+            this.rdbWeiht.Text = "Weight";
+            this.rdbWeiht.UseVisualStyleBackColor = true;
+            this.rdbWeiht.CheckedChanged += new System.EventHandler(this.rdbWeiht_CheckedChanged);
+            // 
+            // FrmConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(316, 189);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbAsia);
+            this.Controls.Add(this.NumInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbWstern);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmConverter";
+            this.Text = "Converter";
+            this.Load += new System.EventHandler(this.FrmConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumInput)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.NumericUpDown NumInput;
+        private System.Windows.Forms.ComboBox cmbWstern;
+        private System.Windows.Forms.ComboBox cmbAsia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbArea;
+        private System.Windows.Forms.RadioButton rdbLength;
+        private System.Windows.Forms.RadioButton rdbWeiht;
+        private System.Windows.Forms.RadioButton rdbTemperature;
+        private System.Windows.Forms.RadioButton rdbValume;
+    }
+}
+
---
+++ Converter/Coverter/Coverter/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coverter
+{
+    public partial class FrmConverter : Form
+    {
+        public FrmConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmConverter_Load(object sender, EventArgs e)
+        {
+            //set Length measurement as a defaul select
+            rdbLength.Checked = true;
+
+            
+            //call convert function to display the value of default
+            lblOutput.Text = ConV(Convert.ToDouble(NumInput.Value), cmbWstern.SelectedItem.ToString(), cmbAsia.SelectedItem.ToString()).ToString();
+           
+        }
+        
+        private double ConV(double input, string ConvFrom, string ConvTo )
+        {
+            double result=0;
+            //Converter for Length
+            if (ConvFrom == "Mile")
+            {
+                if (ConvTo == "Kilometer") result = input * 1.60934;
+                if (ConvTo == "Meter") result = input * 1609.34;
+                if (ConvTo == "Centimeter") result = input * 160934.00;
+                if (ConvTo == "Millimeter") result = input * 1609340.00;
+            }
+            if (ConvFrom == "Yard")
+            {
+                if (ConvTo == "Kilometer") result = input * 0.0009144;
+                if (ConvTo == "Meter") result = input * 0.9144;
+                if (ConvTo == "Centimeter") result = input * 91.44;
+                if (ConvTo == "Millimeter") result = input * 914.4;
+            }
+            if (ConvFrom == "Feet")
+            {
+                if (ConvTo == "Kilometer") result = input * 0.0003048;
+                if (ConvTo == "Meter") result = input * 0.3048;
+                if (ConvTo == "Centimeter") result = input * 30.48;
+                if (ConvTo == "Millimeter") result = input * 304.8;
+            }
+            if (ConvFrom == "Inch")
+            {
+                if (ConvTo == "Kilometer") result = input * 0.0000254;
+                if (ConvTo == "Meter") result = input * 0.0254;
+                if (ConvTo == "Centimeter") result = input * 2.54;
+                if (ConvTo == "Millimeter") result = input * 25.4;
+            }
+
+            //converter for tempurature
+            if (ConvFrom == "Fehrenheit") result =  (input -32) *5/9;
+
+            //Converter for Weight
+            if (ConvFrom == "US-Ton")
+            {
+                if (ConvTo == "Ton") result = input * 0.907185;
+                if (ConvTo == "Killogram") result = input * 907.185;
+                if (ConvTo == "Gram") result = input * 907185;
+                if (ConvTo == "Milligram") result = input * 907200000;
+            }
+            if (ConvFrom == "Stone")
+            {
+                if (ConvTo == "Ton") result = input * 0.00635029;
+                if (ConvTo == "Killogram") result = input * 6.35029;
+                if (ConvTo == "Gram") result = input * 6350.29;
+                if (ConvTo == "Milligram") result = input * 6350000;
+            }
+            if (ConvFrom == "Pound")
+            {
+                if (ConvTo == "Ton") result = input * 0.000453592;
+                if (ConvTo == "Killogram") result = input * 0.453592;
+                if (ConvTo == "Gram") result = input * 453.592;
+                if (ConvTo == "Milligram") result = input * 453592;
+            }
+            if (ConvFrom == "Ounce")
+            {
+                if (ConvTo == "Ton") result = input * 0.00002834949999179;
+                if (ConvTo == "Killogram") result = input * 0.0283495;
+                if (ConvTo == "Gram") result = input * 28.3495;
+                if (ConvTo == "Milligram") result = input * 28349.5;
+            }
+
+            //Converter for Area
+            if (ConvFrom == "Square Mile")
+            {
+                if (ConvTo == "Square kilometer") result = input * 2.58999;
+                if (ConvTo == "Square meter") result = input * 2589990.001027;
+                if (ConvTo == "Hectare") result = input * 258.999;
+                if (ConvTo == "Acre") result = input * 640.00;
+            }
+            if (ConvFrom == "Square Yard")
+            {
+                if (ConvTo == "Square kilometer") result = input * 0.00000083613;
+                if (ConvTo == "Square meter") result = input * 0.836127;
+                if (ConvTo == "Hectare") result = input * 0.000083613;
+                if (ConvTo == "Acre") result = input * 0.000206612;
+            }
+            if (ConvFrom == "Square Feet")
+            {
+                if (ConvTo == "Square kilometer") result = input * 0.000000092903;
+                if (ConvTo == "Square meter") result = input * 0.092903;
+                if (ConvTo == "Hectare") result = input * 0.0000092903;
+                if (ConvTo == "Acre") result = input * 0.000022957;
+            }
+            if (ConvFrom == "Square Inch")
+            {
+                if (ConvTo == "Square kilometer") result = input * 0.00000000064516;
+                if (ConvTo == "Square meter") result = input * 0.00064516;
+                if (ConvTo == "Hectare") result = input * 0.000000064516;
+                if (ConvTo == "Acre") result = input * 0.00000015942;
+            }
+            // Converter Valume:
+
+            if (ConvFrom == "Barrel")
+            {
+                if (ConvTo == "Cubbic meter") result = input * 0.1589873;
+                if (ConvTo == "Liter") result = input * 158.9873;
+                if (ConvTo == "Milliliter") result = input * 158987.3;
+
+            }
+            if (ConvFrom == "Gallon")
+            {
+                if (ConvTo == "Cubbic meter") result = input * 0.00378541;
+                if (ConvTo == "Liter") result = input * 3.78541;
+                if (ConvTo == "Milliliter") result = input * 3785.41;
+            }
+            if (ConvFrom == "Guart")
+            {
+                if (ConvTo == "Cubbic meter") result = input * 0.000946353;
+                if (ConvTo == "Liter") result = input * 0.946353;
+                if (ConvTo == "Milliliter") result = input * 946.353;
+            }
+            if (ConvFrom == "Pint")
+            {
+                if (ConvTo == "Cubbic meter") result = input * 0.000473176;
+                if (ConvTo == "Liter") result = input * 0.473176;
+                if (ConvTo == "Milliliter") result = input * 473.176;
+            }
+
+            return result;
+        }
+
+        //Converter measurement unit when American Combo box change
+        private void cmbAsia_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            if (cmbAsia.SelectedItem != null && cmbWstern.SelectedItem != null)
+            {  
+                lblOutput.Text = ConV(Convert.ToDouble(NumInput.Value), cmbWstern.SelectedItem.ToString(), cmbAsia.SelectedItem.ToString()).ToString();
+            }
+        }
+        
+        //Converter measurement unit when Australia Combo box change
+        private void cmbWstern_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbAsia.SelectedItem != null && cmbWstern.SelectedItem != null)
+            { 
+                lblOutput.Text = ConV(Convert.ToDouble(NumInput.Value), cmbWstern.SelectedItem.ToString(), cmbAsia.SelectedItem.ToString()).ToString();
+            }
+        }
+
+        //Converter measurement unit when Number UpDown box change
+        private void NumInput_ValueChanged(object sender, EventArgs e)
+        {
+            if (cmbAsia.SelectedItem != null && cmbWstern.SelectedItem != null)
+            {
+                lblOutput.Text = ConV(Convert.ToDouble(NumInput.Value), cmbWstern.SelectedItem.ToString(), cmbAsia.SelectedItem.ToString()).ToString();
+            }
+        }
+
+        // Load Measurement Units to ComboBoxes When Type of Measurement Unit Change.
+
+        private void rdbWeiht_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbWstern.Items.Clear();
+            cmbWstern.Items.Add("US-Ton");
+            cmbWstern.Items.Add("Stone");
+            cmbWstern.Items.Add("Pound");
+            cmbWstern.Items.Add("Ounce");
+            cmbWstern.SelectedIndex = 2;
+
+            cmbAsia.Items.Clear();
+            cmbAsia.Items.Add("Ton");
+            cmbAsia.Items.Add("Killogram");
+            cmbAsia.Items.Add("Gram");
+            cmbAsia.Items.Add("Milligram");
+            cmbAsia.SelectedIndex = 1;
+           
+        }
+
+        private void rdbLength_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbWstern.Items.Clear();
+            cmbWstern.Items.Add("Mile");
+            cmbWstern.Items.Add("Yard");
+            cmbWstern.Items.Add("Feet");
+            cmbWstern.Items.Add("Inch");
+            cmbWstern.SelectedIndex = 0;
+
+            cmbAsia.Items.Clear();
+            cmbAsia.Items.Add("Kilometer");
+            cmbAsia.Items.Add("Meter");
+            cmbAsia.Items.Add("Centimeter");
+            cmbAsia.Items.Add("Millimeter");
+            cmbAsia.SelectedIndex = 0;
+        }
+
+        private void rdbArea_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbWstern.Items.Clear();
+            cmbWstern.Items.Add("Square Mile");
+            cmbWstern.Items.Add("Square Yard");
+            cmbWstern.Items.Add("Square Feet");
+            cmbWstern.Items.Add("Square Inch");
+            cmbWstern.SelectedIndex = 2;
+
+            cmbAsia.Items.Clear();
+            cmbAsia.Items.Add("Square kilometer");
+            cmbAsia.Items.Add("Square meter");
+            cmbAsia.Items.Add("Hectare");
+            cmbAsia.Items.Add("Acre");
+            cmbAsia.SelectedIndex = 1;
+          
+        }
+
+        private void rdbTemperature_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbWstern.Items.Clear();
+            cmbWstern.Items.Add("Fehrenheit");
+            cmbWstern.SelectedIndex = 0;
+
+            cmbAsia.Items.Clear();
+            cmbAsia.Items.Add("Celsius");
+            cmbAsia.SelectedIndex = 0;
+            
+        }
+
+        private void rdbValume_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbWstern.Items.Clear();
+            cmbWstern.Items.Add("Barrel");
+            cmbWstern.Items.Add("Gallon");
+            cmbWstern.Items.Add("Guart");
+            cmbWstern.Items.Add("Pint");
+            cmbWstern.SelectedIndex = 1;
+
+            cmbAsia.Items.Clear();
+            cmbAsia.Items.Add("Cubbic meter");
+            cmbAsia.Items.Add("Liter");
+            cmbAsia.Items.Add("Milliliter");
+            cmbAsia.SelectedIndex = 1;
+
+        }
+    }
+}
